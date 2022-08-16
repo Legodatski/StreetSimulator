30ab3de32e5808cd46be7fa5607e87f6e089f115
Legodatski <ovinedev@abv.bg>
2022-08-17 00:12:52 +0300

    tried connecting with sql

---
+++ Context/StreetSimulatorContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Context
+{
+    public class StreetSimulatorContext : DbContext
+    {
+        public StreetSimulatorContext()
+        {
+                
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-HI2ES57;Database=CarDealer;Trusted_Connection=True;");
+            }
+        }
+    }
+}
---
+++ Data/StreetSimulatorContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public namespace Data
+{
+    public class StreetSimulatorContext : DbContext
+    {
+
+    }
+}
